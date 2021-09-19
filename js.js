c5605208df5b041b9f05df7c5926820eaaff28e6
Tanvir04khan <tanvirrrkhan98@gmail.com>
2021-09-20 02:41:19 +0530

    movie data base

--- js.js
+++
@@ -1,8 +0,0 @@
-const noBtn = btns.firstElementChild;
-let noBtnClicked;
-
-function noBtnEventListener() {
-  noBtn.addEventListener("click", clone);
-  noBtnClicked = true;
-  return noBtnClicked;
-}
